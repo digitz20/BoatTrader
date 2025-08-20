7c54452f08c8d6aff8852d94e2426ab1ac33b70c
Alaekeka Ebuka <alaekekaebuka200@gmail.com>
2025-08-20 16:14:25 +0000

    it dosent go to my email when i click it

--- src/components/boat-card.tsx
+++ src/components/boat-card.tsx
@@ -21,7 +21,7 @@ export function BoatCard({ boat }: BoatCardProps) {
     }).format(price);
   };
   
-  const mailtoHref = `mailto:mrarnolddavid23@gmail.com?subject=Inquiry about the ${boat.year} ${boat.make} ${boat.model}&body=I'm interested in getting more information about your ${boat.year} ${boat.make} ${boat.model}. Please contact me. Boat ID: ${boat.id}`;
+  const mailtoHref = `mailto:${boat.seller.email}?subject=Inquiry about the ${boat.year} ${boat.make} ${boat.model}&body=I'm interested in getting more information about your ${boat.year} ${boat.make} ${boat.model}. Please contact me. Boat ID: ${boat.id}`;
 
   return (
     <Card className="overflow-hidden transition-shadow duration-300 hover:shadow-xl">
@@ -73,3 +73,5 @@ export function BoatCard({ boat }: BoatCardProps) {
     </Card>
   );
 }
+
+    

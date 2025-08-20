0137325ce7353f65de76d080be5b9198e56a7fec
Alaekeka Ebuka <alaekekaebuka200@gmail.com>
2025-08-20 14:55:13 +0000

    put it as eth/ERC20, Sol

--- src/components/crypto-payment.tsx
+++ src/components/crypto-payment.tsx
@@ -17,9 +17,9 @@ interface CryptoOption {
 
 const cryptoOptions: CryptoOption[] = [
   { name: "Bitcoin", address: "bc1qqku6e3qxyhlv5fvjaxazt0v5f5mf77lzt0ymm0" },
-  { name: "Ethereum / ERC20", address: "0x328bEaba35Eb07C1D4C82b19cE36A7345ED52C54" },
+  { name: "ETH/ERC20", address: "0x328bEaba35Eb07C1D4C82b19cE36A7345ED52C54" },
   { name: "USDT (TRC20)", address: "THycvE5TKFTLv4nZsq8SJJCYhDmvysSLyk" },
-  { name: "Solana", address: "Gc1Xak8dXJY7h6G8XXMefa9BaiT8VMEsm6G4DXMzyCaX" },
+  { name: "Sol", address: "Gc1Xak8dXJY7h6G8XXMefa9BaiT8VMEsm6G4DXMzyCaX" },
   { name: "BNB Smart Chain", address: "0x328bEaba35Eb07C1D4C82b19cE36A7345ED52C54" },
 ];
 

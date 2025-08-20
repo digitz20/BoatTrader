7334711396b23040d8f829db61bf9420df2e0e18
Alaekeka Ebuka <alaekekaebuka200@gmail.com>
2025-08-20 14:17:46 +0000

    make the time 10seconds and remove the third image address

--- src/components/hero-image-carousel.tsx
+++ src/components/hero-image-carousel.tsx
@@ -8,7 +8,6 @@ import { cn } from '@/lib/utils';
 const images = [
   'https://servedby.boatsgroup.com/e061c2b61/?libBID=4433508',
   'https://servedby.boatsgroup.com/e061c2b61/?libBID=4463621',
-  'https://images.boattrader.com/resize/1/11/13/9031113_20230906080646186_1_LARGE.jpg?w=800&h=600&t=1736521177000&exact',
   'https://servedbyadbutler.com/getad.img/;libID=4598090',
   'https://images.boatsgroup.com/images/1/upload/kadeykrogennewhero9123.png',
   'https://images.boatsgroup.com/images/1/upload/hanoverhero41124.png',
@@ -21,7 +20,7 @@ export function HeroImageCarousel() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 30000); // Change image every 30 seconds
+    }, 10000); // Change image every 10 seconds
 
     return () => clearInterval(intervalId);
   }, []);

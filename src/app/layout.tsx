8b9f18d8041b29c20397d73699daced4c7ea0402
Alaekeka Ebuka <alaekekaebuka200@gmail.com>
2025-08-20 13:22:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Tinos:wght@700&display=swap" rel="stylesheet" />
-      </head>
       <body className="font-body antialiased">
         <div className="flex min-h-screen flex-col">
           <Header />
